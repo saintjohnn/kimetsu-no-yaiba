50f5eb99ec650ce7a38fa634764b323d7e077d02
John <saintjohn608@gmail.com>
2024-06-25 18:35:29 -0300

    feat: add dark and light modes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const headerNavigation = document.querySelector(".header__navigation");
 const headerNavigationCloseButton = document.querySelector(
   ".header__navigation-close-button"
 );
+const modesLight = document.querySelector(".modes__light");
+const modes = document.querySelector(".modes");
 
 if (headerMenuButton) {
   headerMenuButton.addEventListener("click", () => {
@@ -15,3 +17,21 @@ if (headerNavigationCloseButton) {
     headerNavigation.classList.remove("show-menu");
   });
 }
+
+modesLight.addEventListener("click", function () {
+  if (modesLight.classList.contains("ri-sun-line")) {
+    modesLight.classList.remove("ri-sun-line");
+    modesLight.classList.add("ri-contrast-2-fill");
+  } else {
+    modesLight.classList.remove("ri-contrast-2-fill");
+    modesLight.classList.add("ri-sun-line");
+  }
+});
+
+modes.addEventListener("click", () => {
+  if (document.body.classList.contains("modes--light")) {
+    document.body.classList.remove("modes--light");
+  } else {
+    document.body.classList.add("modes--light");
+  }
+});
