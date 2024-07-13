b9253dc061ce1a1083acdb5665ee8307a4f0fdde
John <saintjohn608@gmail.com>
2024-07-13 19:02:37 -0300

    fix:adjust code

--- index.js
+++ index.js
@@ -101,7 +101,8 @@ if (btnLogout) {
 
 if (btnUsernameText && btnUsernameText.innerText.length > 11) {
 	const text = btnUsernameText.textContent;
+	const text1 = text.slice(0, 10);
 	const text2 = text.slice(10);
 
-	text2.classList.add("opacity");
+	btnUsernameText.innerHTML = `${text1}<span class="opacity">${text2}</span>`;
 }
