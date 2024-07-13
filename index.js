ba0f0a99861dd995db17173f36f30bb4ba86a217
John <saintjohn608@gmail.com>
2024-07-13 20:00:52 -0300

    fix:adjust code

--- index.js
+++ index.js
@@ -100,9 +100,10 @@ if (btnLogout) {
 }
 
 if (btnUsernameText && btnUsernameText.innerText.length > 11) {
-	const text = btnUsernameText.textContent;
-	const text1 = text.slice(0, 11);
-	const text2 = text.slice(11);
+	btnUsernameText.classList.add("degrade");
+	//const text = btnUsernameText.textContent;
+	//const text1 = text.slice(0, 11);
+	//const text2 = text.slice(11);
 
-	btnUsernameText.innerHTML = `${text1}<span class="degrade">${text2}</span>`;
+	//btnUsernameText.innerHTML = `${text1}<span class="degrade">${text2}</span>`;
 }
