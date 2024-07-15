79b5174252b97755654b8457a65663ab30f684e0
John <saintjohn608@gmail.com>
2024-07-15 20:35:41 -0300

    feat: final structure

--- src/js/pages/login/login.js
+++ src/js/pages/login/login.js
@@ -1,28 +1,28 @@
 const inputContainerUserInputEmail = document.querySelector("[name=email]");
 
 const inputContainerUserInputPassword =
-  document.querySelector("[name=password]");
+	document.querySelector("[name=password]");
 
 const emailLabel = document.querySelector("#email-label");
 
 const passwordLabel = document.querySelector("#password-label");
 
 inputContainerUserInputEmail.addEventListener("focus", () => {
-  emailLabel.classList.add("input-container__user-input-event");
-  setTimeout(() => {
-    inputContainerUserInputEmail.setAttribute(
-      "placeholder",
-      "Insira seu email"
-    );
-  }, 15);
+	emailLabel.classList.add("input-container__user-input-event");
+	setTimeout(() => {
+		inputContainerUserInputEmail.setAttribute(
+			"placeholder",
+			"Insira seu email",
+		);
+	}, 15);
 });
 
 inputContainerUserInputPassword.addEventListener("focus", () => {
-  passwordLabel.classList.add("input-container__user-input-event");
-  setTimeout(() => {
-    inputContainerUserInputPassword.setAttribute(
-      "placeholder",
-      "Insira sua senha"
-    );
-  }, 15);
+	passwordLabel.classList.add("input-container__user-input-event");
+	setTimeout(() => {
+		inputContainerUserInputPassword.setAttribute(
+			"placeholder",
+			"Insira sua senha",
+		);
+	}, 15);
 });
